6c73ea2cda62117d150a7293d61769f739fe71bc
Ben Seymour <bseymour@users.noreply.github.com>
2025-07-20 18:09:46 +0100

    adding in contributing to agent

--- utils/agent.ts
+++ utils/agent.ts
@@ -1,9 +1,13 @@
+import { generateText, stepCountIs, tool } from "ai";
+import z from "zod/v4";
+import fs from "fs";
+
 export async function codingAgent(prompt: string) {
   const result = await generateText({
     model: "openai/gpt-4.1-mini",
     prompt,
     system:
-      "You are a coding agent. You will be working with js/ts projects. Your responses must be concise. Always start by listing all the files in the current directory.",
+      "You are a coding agent. You will be working with js/ts projects. Your responses must be concise.",
     stopWhen: stepCountIs(10),
     tools: {
       list_files: tool({
@@ -34,10 +38,62 @@ export async function codingAgent(prompt: string) {
           }
         },
       }),
-      read_file: tool({ 
+      read_file: tool({
         description:
-          "Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.", 
+          "Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.",
+        inputSchema: z.object({
+          path: z
+            .string()
+            .describe("The relative path of a file in the working directory."),
+        }),
+        execute: async ({ path }) => {
+          try {
+            console.log(`Reading file at '${path}'`);
+            const output = fs.readFileSync(path, "utf-8");
+            return { path, output };
+          } catch (error) {
+            console.error(`Error reading file at ${path}:`, error.message);
+            return { path, error: error.message };
+          }
+        },
+      }),
+      edit_file: tool({ 
+        description:
+          "Make edits to a text file or create a new file. Replaces 'old_str' with 'new_str' in the given file. 'old_str' and 'new_str' MUST be different from each other. If the file specified with path doesn't exist, it will be created.", 
           inputSchema: z.object({ 
-            path: z 
+            path: z.string().describe("The path to the file"), 
+            old_str: z 
               .string() 
-              .describe("The relative path of a file in the working directory."), 
+              .nullable() 
+              .describe( 
+                "Text to search for - must match exactly and must only have one match exactly", 
+              ), 
+            new_str: z.string().describe("Text to replace old_str with"), 
+          }), 
+          execute: async ({ path, old_str, new_str }) => { 
+            try { 
+              const fileExists = fs.existsSync(path); 
+              if (fileExists && old_str !== null) { 
+                console.log(`Editing file '${path}'`); 
+                const fileContents = fs.readFileSync(path, "utf-8"); 
+                const newContents = fileContents.replace(old_str, new_str); 
+                fs.writeFileSync(path, newContents); 
+                return { path, success: true, action: "edit" }; 
+              } else { 
+                console.log(`Creating file '${path}'`); 
+                fs.writeFileSync(path, new_str); 
+                return { path, success: true, action: "create" }; 
+              } 
+            } catch (e) { 
+              console.error(`Error editing file ${path}:`, e); 
+              return { error: e, success: false }; 
+            } 
+          }, 
+      }), 
+    },
+  });
+
+  return {
+    response: result.text,
+  };
+}
--- utils/test.ts
+++ utils/test.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: ".env.local" });
 
-codingAgent("Tell me how the agent.ts works")
+// codingAgent("Tell me how the agent.ts works")
+codingAgent("Please can you add the contributing section to the readme")
   .then(console.log)
   .catch(console.error);
