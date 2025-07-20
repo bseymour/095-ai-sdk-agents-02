d2c091610d6ab4d7da9911a027a02f7aec06512a
Ben Seymour <bseymour@users.noreply.github.com>
2025-07-20 18:07:13 +0100

    progression -

--- utils/test.ts
+++ utils/test.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: ".env.local" });
 
-codingAgent("Tell me about this project")
+codingAgent("Tell me how the agent.ts works")
   .then(console.log)
   .catch(console.error);
